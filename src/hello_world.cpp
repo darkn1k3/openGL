e93aa93315702348f662ccbfbef2a57a5878b47b
mark <secret678@gmail.com>
2020-07-17 15:30:14 +0300

    changing color triangle using uniform variable;

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <math.h>
 
 // prototypes
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
@@ -22,11 +23,12 @@ const char *vertexShaderSource = "#version 330 core\n"
                                  "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
-                                    "out vec4 FragColor;\n"
-                                    "void main()\n"
-                                    "{\n"
-                                    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                    "}\0";
+                                   "out vec4 FragColor;\n"
+                                   "uniform vec4 color;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "    FragColor = color;\n"
+                                   "}\0";
 
 int main(int argc, char **argv)
 {
@@ -86,27 +88,32 @@ int main(int argc, char **argv)
     glDeleteShader(fragmentShader);
 
     // Set up vertex data and configure attributes.
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f};
+    float vertices1[] = {
+        0.5f, 0.5f, 0.0f,
+        0.5f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.0f};
+
+    // unsigned int indices[] = {0, 1, 2, 2, 3, 4};
 
     // generate 1 vertex buffer by generating its unique reference ID
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-
+    unsigned int VBOs, VAOs, EBO;
+    glGenVertexArrays(1, &VAOs);
+    glGenBuffers(1, &VBOs);
+    // glGenBuffers(1, &EBO);
+        
     // bind the vertex buffer type and load the vertices into the allocated bound buffer in the GPUs memory
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+    glBindVertexArray(VAOs);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
     // link vertex input data to vertex shader attributes
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
-    // unbind vertex buffer and vertex array because we already binded and registered the
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+    // unbind vertex buffer and vertex array because we already binded and "registered them"
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
@@ -120,10 +127,22 @@ int main(int argc, char **argv)
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // change color in runtime using uniform variable
+        float timeVal = glfwGetTime();
+        float greenVal, redVal, blueVal;
+        greenVal = (sin(timeVal) / 2.0f) + 0.5f;
+        redVal = (cos(timeVal) / 2.0f) + 0.5f;
+        blueVal = (sin(timeVal) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "color");
+        glUniform4f(vertexColorLocation, redVal, greenVal, blueVal, 1.0f);
+
         // draw call
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO); // not needed to bind every frame since we have only one
+
+        glBindVertexArray(VAOs); // not needed to bind every frame since we have only one
         glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         glfwSwapBuffers(window);
